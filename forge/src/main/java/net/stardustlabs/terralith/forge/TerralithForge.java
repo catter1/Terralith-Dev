06b8a37cb99a1f23deb91ce7e73783062f2cb8e5
catter1 <63272345+catter1@users.noreply.github.com>
2023-05-09 12:43:09 +0200

    Updated repo

--- forge/src/main/java/net/stardustlabs/terralith/forge/TerralithForge.java
+++ forge/src/main/java/net/stardustlabs/terralith/forge/TerralithForge.java
@@ -1,15 +1,73 @@
 package net.stardustlabs.terralith.forge;
 
-import dev.architectury.platform.forge.EventBuses;
+import net.stardustlabs.terralith.forge.config.configs.CommentedConfig;
+import net.stardustlabs.terralith.forge.config.r.ConfigUtil;
+import net.stardustlabs.terralith.forge.utils.Util;
+
 import net.stardustlabs.terralith.Terralith;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.Mod.Metadata;
+import net.minecraftforge.fml.common.ModContainer;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.eventhandler.EventBusSubscriber;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-@Mod(Terralith.MOD_ID)
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(TerralithForge.MODID)
 public class TerralithForge {
-    public TerralithForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(Terralith.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+
+    public static final String MODID = "terralith";
+    public static final String NAME = "Terralith";
+    public static final String VERSION = "1.0.0"; // Replace with the appropriate version for your mod
+    public static final Logger LOGGER = LogManager.getLogger(ConfigUtil.MODID);
+    public static final String minTerraBlenderVersion = "2.2.0.154";
+    
+    @Instance(TerralithForge.MODID)
+    public static TerralithForge instance;
+
+    @Metadata(TerralithForge.MODID)
+    public static ModContainer container;
+
+    public static Mode currentMode = Util.getMode(CommentedConfig.getConfig().terrablender());
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        // Pre-initialization code
         Terralith.init();
+        LOGGER.info("Loading Terralith");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Initialization code
+    }
+
+    public static boolean isTerraBlenderLoaded() {
+        if (Loader.isModLoaded("terrablender")) {
+            ModContainer modContainer = Loader.instance().getIndexedModList().get("terrablender");
+            Version version = modContainer.getProcessedVersion();
+            Version min;
+            try {
+                min = VersionParser.parseVersionReference(minTerraBlenderVersion);
+            } catch (VersionParsingException e) {
+                throw new RuntimeException(e);
+            }
+            return version.compareTo(min) >= 0;
+        }
+        return false;
+    }
+
+    public enum Mode {
+        TERRABLENDER,
+        DEFAULT
     }
 }
